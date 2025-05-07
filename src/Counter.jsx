bd95595601811a69871654e65c49463b3ef74062
MinJeong <mineun0517@naver.com>
2025-05-07 19:27:12 +0900

    Counter 실습

---
+++ src/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  const btnStyle =
+    "px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl";
+
+  return (
+    <div className="flex flex-col bg-gray-100 p-10">
+      <h1 className="text-3xl font-bold mb-4">Counter</h1>
+      <p className="text-2xl mb-6">현재 값: {count}</p>
+      <div className="flex gap-2">
+        <button onClick={handleIncrease} className={btnStyle}>
+          +1
+        </button>
+        <button onClick={handleDecrease} className={btnStyle}>
+          -1
+        </button>
+        <button onClick={handleReset} className={btnStyle}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
